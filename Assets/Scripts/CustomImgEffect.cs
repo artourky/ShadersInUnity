dbf7dc3f765812f017f7ec33c413dee34c8a7f95
Arshd <arshad@sort6.com>
2017-12-25 17:26:14 +0200

    prepare for Image effect

---
+++ Assets/Scripts/CustomImgEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomImgEffect : MonoBehaviour {
+
+	public Material effectMat;
+
+	//void OnRenderImage(RenderTexture source, RenderTexture destination)
+	//{
+	//	//Graphics.Blit(source, destination, effectMat); 
+	//}
+}
